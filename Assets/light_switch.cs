8f69ca368c2322e4a6e3fe90135d257edb0154f4
polymini <polyphonic13@gmail.com>
2015-03-28 14:54:18 -0400

    light_switch added to player lamp; hot_water01 lamp added at bottom of room01a

--- Assets/light_switch.cs
+++ Assets/light_switch.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class light_switch : MonoBehaviour {
 
+	private Light _bulb;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake() {
+		_bulb = GetComponent<Light>();
+		_bulb.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.L)) {
+			_bulb.enabled = !_bulb.enabled;
+		}
 	}
 }
