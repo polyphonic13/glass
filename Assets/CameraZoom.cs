1e3d7166b3130d1964d76d46229f86adbfe05324
polymini <polyphonic13@gmail.com>
2015-03-29 12:02:39 -0400

    abondoned bubbling water in room01a; room01a mesh normal artifacts fixed; CameraZoom script added

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+	public float zoom = 10;
+
+	Camera _camera;
+	float _normal;
+	float _smooth = 0.5f;
+	bool _isZoomed = false;
+
+	// Use this for initialization
+	void Start () {
+		_camera = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+		_normal = _camera.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Z)) {
+			zoomCamera();
+		}
+	}
+
+	void zoomCamera() {
+		if(_isZoomed) {
+//			_camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, _normal, Time.deltaTime / _smooth);
+			_camera.fieldOfView = _normal;
+		} else {
+//			_camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, zoom, Time.deltaTime / _smooth);
+			_camera.fieldOfView = zoom;
+		}
+		_isZoomed = !_isZoomed;
+	}
+}
