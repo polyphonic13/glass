f7abf9bff984961572ba3e5f866aef21eeaf8435
polymini <polyphonic13@gmail.com>
2015-04-04 12:38:31 -0400

    environment colliders added for room03a; crystals added to room03a

--- Assets/SampleAssets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/SampleAssets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -89,7 +89,7 @@ namespace UnitySampleAssets.Characters.FirstPerson
         private void PlayLandingSound()
         {
             m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
+            // m_AudioSource.Play();
             m_NextStep = m_StepCycle + .5f;
         }
 
@@ -137,7 +137,7 @@ namespace UnitySampleAssets.Characters.FirstPerson
         private void PlayJumpSound()
         {
             m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
+            // m_AudioSource.Play();
         }
 
 
@@ -170,7 +170,7 @@ namespace UnitySampleAssets.Characters.FirstPerson
             // excluding sound at index 0
             int n = Random.Range(1, m_FootstepSounds.Length);
             m_AudioSource.clip = m_FootstepSounds[n];
-            m_AudioSource.PlayOneShot(m_AudioSource.clip);
+            // m_AudioSource.PlayOneShot(m_AudioSource.clip);
             // move picked sound to index 0 so it's not picked next time
             m_FootstepSounds[n] = m_FootstepSounds[0];
             m_FootstepSounds[0] = m_AudioSource.clip;
---
+++ Assets/scripts/FirstPersonMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonMovement : MonoBehaviour {
+
+	enum _movementStates { normal, climb, swim };
+	static _movementStates _currentMovementType;
+
+	void Awake() {
+		_currentMovementType = _movementStates.normal;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// determine move state first
+
+		switch(_currentMovementType) {
+		case _movementStates.normal:
+			normalMove();
+			break;
+		case _movementStates.climb:
+			climbMove();
+			break;
+		case _movementStates.swim:
+			swimMove();
+			break;
+		}
+	}
+
+	void normalMove() {
+
+	}
+
+	void climbMove() {
+
+	}
+
+	void swimMove() {
+
+	}
+	
+}
