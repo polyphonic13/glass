3c5ca77dddc90dc2bd88a3e15dc897dfa0727a06
Paul Hoover <polyphonic13@gmail.com>
2015-09-27 14:25:11 -0400

    work on bedroom_n bed

--- Assets/scripts/RoomTrigger.cs
+++ Assets/scripts/RoomTrigger.cs
@@ -5,7 +5,7 @@ public class RoomTrigger : MonoBehaviour {
 	public string _roomName;
 
 	void OnTriggerEnter(Collider tgt) {
-		Debug.Log("RoomTrigger/OnTriggerEnter, _roomName = " + _roomName);
+//		Debug.Log("RoomTrigger/OnTriggerEnter, _roomName = " + _roomName);
 		EventCenter.Instance.EnterRoom(_roomName);
 	}
 
