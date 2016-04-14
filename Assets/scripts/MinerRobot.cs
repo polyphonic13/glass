6bd7efebaad4746e1e2ea8e5f3663e4901840b9d
Paul Hoover <polyphonic13@gmail.com>
2016-04-14 11:26:16 -0400

    simplifying miner robot with single movement animation

--- Assets/scripts/MinerRobot.cs
+++ Assets/scripts/MinerRobot.cs
@@ -6,30 +6,39 @@ public class MinerRobot : MonoBehaviour {
 	public AnimationClip[] movementAnimations;
 	public Transform[] movementBones; 
 
-	private int _currentMovement = 0;
+	private int _currentMovement = -1;
 
 	private Animation _animation; 
 
-	public void PlayNextMovement() {
+	public void StartMovement() {
+		Debug.Log ("MinerRobot/PlayNextMovement, _currentMovement = " + _currentMovement + " length = " + movementAnimations.Length);
+
 		if (_currentMovement < movementAnimations.Length - 1) {
 			_currentMovement++;
 		} else {
 			_currentMovement = 0;
 		}
-		Debug.Log ("MinerRobot/PlayNextMovement, _currentMovement = " + _currentMovement);
 		string clip = movementAnimations [_currentMovement].name;
 		Transform bone = movementBones [_currentMovement];
 		Debug.Log ("clip = " + clip);
-		if(bone != null) {
-			_animation [clip].AddMixingTransform(bone);
-		}
-		_animation [clip].wrapMode = WrapMode.Once;
+//		if(bone != null) {
+//			_animation [clip].AddMixingTransform(bone);
+//		}
+		_animation [clip].wrapMode = WrapMode.Loop;
 		_animation.Play(clip);
+	}
+
+	public void StartHands() {
+
+	}
+
+	public void StopHands() {
+
 	}
 
 	void Awake() {
 		_animation = GetComponent<Animation> ();
-		PlayNextMovement ();
+		StartMovement();
 	}
 	
 	void Update () {
