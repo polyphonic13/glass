df93bd265768c442b77763d546fa9fdd0f4b04be
Paul Hoover <polyphonic13@gmail.com>
2016-07-17 16:55:43 -0400

    fixing proxmity bs -- added to Polyworks

---
+++ Assets/scripts/_polyworks/items/ProximityController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Polyworks {
+	public class ProximityController : MonoBehaviour {
+
+		public float interactDistance = 2f;
+		public Transform target;
+		public bool isTargetPlayer; 
+
+		private bool _wasJustFocused;
+		private Item _item;
+	
+		public void SetFocus(bool isFocused) {
+			if (isFocused) {
+				if (!_wasJustFocused) {
+					EventCenter.Instance.ChangeItemProximity(_item, true);
+					_wasJustFocused = true;
+				}
+			} else if (_wasJustFocused) {
+				EventCenter.Instance.ChangeItemProximity(_item, false);
+				_wasJustFocused = false;
+			}
+		}
+
+		public bool Check() {
+			bool isInProximity = false;
+			var difference = Vector3.Distance(target.position, transform.position);
+			if(difference < interactDistance) {
+				isInProximity = true;
+				EventCenter.Instance.ChangeItemProximity(_item, isInProximity);
+				_wasJustFocused = true;
+			} else if(_wasJustFocused) {
+				EventCenter.Instance.ChangeItemProximity(_item, isInProximity);
+				_wasJustFocused = false;
+			}
+			return isInProximity;
+		}
+	
+		private void Awake() {
+			_item = gameObject.GetComponent<Item> ();
+			if (isTargetPlayer) {
+				target = GameObject.Find ("player").transform;
+			}
+		}
+	}
+}
