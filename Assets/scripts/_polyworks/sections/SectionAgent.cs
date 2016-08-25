439e0b327f5ca66fbd187f1b106c807ccfab94e2
Paul Hoover <polyphonic13@gmail.com>
2016-08-25 12:33:28 -0400

    beginning to restore room02d lift (WIP)

--- Assets/scripts/_polyworks/sections/SectionAgent.cs
+++ Assets/scripts/_polyworks/sections/SectionAgent.cs
@@ -5,8 +5,8 @@ namespace Polyworks {
 	public class SectionAgent : Reaction
 	{
 		public void ToggleEnabled(bool isEnabled) {
-//			Debug.Log ("SectionAgent[" + this.name + "]/ToggleEnabled, isEnabled = " + isEnabled);
 			if (isEnabled) {
+				Debug.Log ("SectionAgent[" + this.name + "]/ToggleEnabled, isEnabled = " + isEnabled);
 				this.gameObject.SendMessage ("Enable");
 			} else {
 				this.gameObject.SendMessage ("Disable");
