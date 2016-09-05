b2920d86429380ed0a4cf294ba6da18d01a9b083
Paul Hoover <polyphonic13@gmail.com>
2016-09-05 13:08:45 -0400

    house_floor02a: flashlight functioning again, collected from bedroom_nw_dresser, middle drawer, using EventCenter.OnActuateFlashlight for toggle on/off

---
+++ Assets/scripts/_items/house_floor02/Flashlight.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Polyworks;
+
+public class Flashlight : CollectableItem {
+	public string parentName;
+	public GameObject model;
+	private Light _bulb;
+
+	public void OnActuateFlashlight() {
+		Debug.Log ("Flashlight/OnActuateFlashlight");
+		_bulb.enabled = !_bulb.enabled;
+	}
+
+	public override void Actuate() {
+		Debug.Log ("Flashlight/Actuate");
+		model.SetActive (false);
+
+		GameObject playerHead = GameObject.Find (parentName);
+		this.transform.parent = playerHead.transform;
+	}
+
+	private void Awake() {
+		_bulb = gameObject.GetComponent<Light>();
+		_bulb.enabled = false;
+
+		EventCenter.Instance.OnActuateFlashlight += OnActuateFlashlight;
+	}
+
+	private void OnDestroy() {
+		EventCenter ec = EventCenter.Instance;
+		if (ec != null) {
+			ec.OnActuateFlashlight -= OnActuateFlashlight;
+		}
+	}
+}
