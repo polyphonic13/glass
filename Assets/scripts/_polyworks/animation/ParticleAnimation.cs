3abcf368c4ff0bb9f0c4f0e1134f43c83957cfcf
Paul Hoover <polyphonic13@gmail.com>
2016-09-11 08:11:18 -0400

    ParticalAnimation added and implemented on portal in house_floor02a; removed CollidableChild, CollidableParent, PortalController and PortalCrystal

---
+++ Assets/scripts/_polyworks/animation/ParticleAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Polyworks {
+	public class ParticleAnimation : AnimationAgent
+	{
+		public ParticleSystem[] systems;
+		public bool isStartActive; 
+
+		public override void Play(string clip = "") {
+			base.Play();
+			_toggleSystems (true);
+		}
+
+		public override void Pause() {
+			base.Pause ();
+			_toggleSystems (false);
+		}
+
+		public override void Resume() {
+			base.Resume ();
+			_toggleSystems (true);
+		}
+
+		public override bool GetIsActive() {
+			return isActive;
+		}
+	
+		private void Awake() {
+			_toggleSystems (isStartActive);
+		}
+
+		private void _toggleSystems(bool enable) {
+			for (var i = 0; i < systems.Length; i++) {
+				systems [i].enableEmission = enable;
+			}
+		}
+	}
+}
+
