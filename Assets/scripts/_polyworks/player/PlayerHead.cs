0298ea77c524d58fd0a5b77c144ed4936b51b4c8
Paul Hoover <polyphonic13@gmail.com>
2017-01-09 14:15:03 -0500

    puzzle_inspector / puzzles: updates to puzzle locations; puzzle01_0222 added (TBD); player: crosshair_ui positioned closer to camera in order to resolve issue where player in cramped areas

--- Assets/scripts/_polyworks/player/PlayerHead.cs
+++ Assets/scripts/_polyworks/player/PlayerHead.cs
@@ -48,31 +48,31 @@ namespace Polyworks {
 			}
 		}
 
-		bool IsLookingAtObject(Transform obj, Transform targetObj) {
-			Vector3 direction = targetObj.position - obj.position;
-
-			float ang = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-			//current angle in degrees
-			float anglecurrent = obj.eulerAngles.z;
-			float checkAngle = 0;
-
-			//Checking to see what quadrant current angle is at
-			if (anglecurrent > 0 && anglecurrent <= 90) {
-				checkAngle = ang - 90;
-			}
-			if (anglecurrent > 90 && anglecurrent <= 360) {
-				checkAngle = ang + 270;
-			}
-
-			//If current angle is equal to the angle that I need to be at to look at the object, return true
-			//It is possible to do "if (checkAngle == anglecurrent)" but some times you don't get an exact match so do something like below to have some error range.
-
-			if (anglecurrent <= checkAngle + 0.5f && anglecurrent >= checkAngle - 0.5f) {
-				return true;  
-			} else {
-				return false;
-			}
-		}
+//		bool IsLookingAtObject(Transform obj, Transform targetObj) {
+//			Vector3 direction = targetObj.position - obj.position;
+//
+//			float ang = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//
+//			//current angle in degrees
+//			float anglecurrent = obj.eulerAngles.z;
+//			float checkAngle = 0;
+//
+//			//Checking to see what quadrant current angle is at
+//			if (anglecurrent > 0 && anglecurrent <= 90) {
+//				checkAngle = ang - 90;
+//			}
+//			if (anglecurrent > 90 && anglecurrent <= 360) {
+//				checkAngle = ang + 270;
+//			}
+//
+//			//If current angle is equal to the angle that I need to be at to look at the object, return true
+//			//It is possible to do "if (checkAngle == anglecurrent)" but some times you don't get an exact match so do something like below to have some error range.
+//
+//			if (anglecurrent <= checkAngle + 0.5f && anglecurrent >= checkAngle - 0.5f) {
+//				return true;  
+//			} else {
+//				return false;
+//			}
+//		}
 	}
 }
