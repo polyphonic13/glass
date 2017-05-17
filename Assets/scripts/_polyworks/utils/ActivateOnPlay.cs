4dfe8cfd480b2d5734f2ad29b80b557c2810a32d
Paul Hoover <polyphonic13@gmail.com>
2017-05-16 22:24:24 -0400

    ActivateOnPlay utility added and implemented in house_sceouse_scene02a with floor01, floor03, floor04 and trees objects addd

---
+++ Assets/scripts/_polyworks/utils/ActivateOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateOnPlay : MonoBehaviour {
+
+	public GameObject[] objects; 
+
+	void Awake () {
+		for (int i = 0; i < objects.Length; i++) {
+			objects [i].SetActive (true);
+		}
+	}
+}
