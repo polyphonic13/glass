2995b319fc21527c68bf2d3f6fab493867e80969
Paul Hoover <polyphonic13@gmail.com>
2017-07-01 10:12:43 -0400

    ObjectIcon added to repo

---
+++ Assets/scripts/_polyworks/input/ObjectIcon.cs
@@ -0,0 +1,25 @@
+﻿namespace Polyworks {
+	using UnityEngine;
+	using System.Collections;
+
+	public class ObjectIcon : MonoBehaviour
+	{
+		public GameObject target;
+
+		public void SetTarget(GameObject target) {
+			this.target = target;
+		}
+
+		public void Enable() {
+			if (target != null) {
+				target.SetActive (true);
+			}
+		}
+
+		public void Disable() {
+			if (target != null) {
+				target.SetActive (false);
+			}
+		}
+	}
+}
