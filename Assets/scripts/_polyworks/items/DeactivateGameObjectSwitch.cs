2cf0083cd42cbfe1ee8ba062bee3d71052a99493
Paul Hoover <polyphonic13@gmail.com>
2017-07-27 11:42:33 -0400

    DeactiveGameObjectSwtich: added and implemented in puzzle04 success lever collider

---
+++ Assets/scripts/_polyworks/items/DeactivateGameObjectSwitch.cs
@@ -0,0 +1,20 @@
+﻿namespace Polyworks {
+	using UnityEngine;
+	using System.Collections;
+
+	public class DeactivateGameObjectSwitch : Switch
+	{
+		public bool isTargetSelf;
+
+		public GameObject target; 
+
+		public override void Actuate ()
+		{
+			Debug.Log ("DeactivateGameObjectSwitch[" + this.name + "]/Actuate, target = " + target);
+			base.Actuate ();
+			if (target != null) {
+				target.SetActive (false);
+			}
+		}
+	}
+}
