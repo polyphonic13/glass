a2040189eb8c9197385efd4e976faf81749cf114
Paul Hoover <polyphonic13@gmail.com>
2017-07-30 20:14:27 -0400

    puzzle05 implemented in bedroom_se (slight issue with plugs not appearing in highlights; ChildMaterialInitializer add and implemented in puzzle05 to set material of all wires highlights

---
+++ Assets/scripts/_polyworks/utils/ChildMaterialInitializer.cs
@@ -0,0 +1,43 @@
+﻿namespace Polyworks {
+	using UnityEngine;
+	using System.Collections;
+
+	public class ChildMaterialInitializer : MonoBehaviour
+	{
+		public Material material;
+
+		public string path = ""; 
+		public bool isRecursive; 
+
+		private void Awake() {
+			if (path != "") {
+				Transform target = transform.Find (path);
+				if (target != null) {
+					_setMaterialOnTransformChildren (target);
+				}
+			}
+		}
+
+		private void _setMaterialOnTransformChildren(Transform target) {
+			Debug.Log (" _setMaterialOnTransformChildren, target = " + target.gameObject.name);
+			foreach (Transform child in target) {
+				_setMaterialOnTransform (child);
+
+				if (isRecursive) {
+					_setMaterialOnTransformChildren (child);
+				}
+			}
+		}
+
+		private void _setMaterialOnTransform(Transform child) {
+			Debug.Log ("  _setMaterialOnTransform, child = " + child.gameObject.name);
+			GameObject go = child.gameObject;
+			if (go != null) {
+				Renderer renderer = go.GetComponent<Renderer> ();
+				if (renderer != null) {
+					renderer.material = material;
+				}
+			}
+		}
+	}
+}
