c9b60af918db7cc1370d292f3b3997dd692e91ce
Paul Hoover <polyphonic13@gmail.com>
2017-08-18 13:45:11 -0400

    puzzle02_combination text wallpaper improvements; wall paper puzzle combinations fixed in prefab to match rooms

--- Assets/scripts/_polyworks/core/GameObjectRotator.cs
+++ Assets/scripts/_polyworks/core/GameObjectRotator.cs
@@ -7,6 +7,8 @@
 		public Transform[] targets;
 		public Vector3[] rotations;
 
+		public float turningRate = 0; 
+
 		public bool isLooping = true; 
 		public bool isLogOn;
 
@@ -35,8 +37,18 @@
 		private void _rotateTargets(Vector3 rotation) {
 			for (int i = 0; i < targets.Length; i++) {
 				_log (" rotating targets[" + i + "]: " + targets [i]);
-				targets [i].Rotate (rotation);
+				if (turningRate == 0) {
+					targets [i].Rotate (rotation);
+				} else {
+					_animateRotateTarget (rotation, targets [i]);
+				}
 			}
+		}
+		 
+		private void _animateRotateTarget(Vector3 rotation, Transform target) {
+			Quaternion targetRotation = Quaternion.Euler(rotation);
+			_log ("GameObjectRotator/_animateRotateTarget, rotation = " + rotation + ", targetRotation = " + targetRotation + ", transform.rotation = " + target.rotation);
+			target.rotation = Quaternion.RotateTowards (target.rotation, targetRotation, turningRate * Time.deltaTime);
 		}
 
 		private void _log(string message) {
