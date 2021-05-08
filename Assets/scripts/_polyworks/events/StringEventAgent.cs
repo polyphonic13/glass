79391041b87db4151e0e66be46ef245fc7125291
Paul Hoover <polyphonic13@gmail.com>
2021-05-08 10:40:24 -0400

    procedural-sky day/night speed slowed down; house_second_floor: point lights inensity to 1.0

--- Assets/scripts/_polyworks/events/StringEventAgent.cs
+++ Assets/scripts/_polyworks/events/StringEventAgent.cs
@@ -1,62 +1,77 @@
 using UnityEngine;
-using System.Collections;
 
-namespace Polyworks {
-	public class StringEventAgent : MonoBehaviour {
-		public string eventType = "";
-		public string eventValue = ""; 
-		
-		public bool isLogOn = false; 
+namespace Polyworks
+{
+    public class StringEventAgent : MonoBehaviour
+    {
+        public string eventType = "";
+        public string eventValue = "";
 
-		private bool _isListenersAdded = false;
+        public bool isLogOn = false;
 
-		public void OnStringEvent(string type, string value) {
-			Log ("StringEventAgent[" + this.name + "]/OnStringEvent, type = " + type + ", eventType = " + eventType + ", value = " + value + ", eventValue = " + eventValue);
-			if (type == eventType && value == eventValue) {
-				SendMessage ("Actuate", null, SendMessageOptions.DontRequireReceiver);
-			}
-		}
+        private bool _isListenersAdded = false;
 
-		public void Enable() {
-			_addListeners ();
-		}
+        public void OnStringEvent(string type, string value)
+        {
+            Log("StringEventAgent[" + this.name + "]/OnStringEvent, type = " + type + ", eventType = " + eventType + ", value = " + value + ", eventValue = " + eventValue);
+            if (type == eventType && value == eventValue)
+            {
+                SendMessage("Actuate", null, SendMessageOptions.DontRequireReceiver);
+            }
+        }
 
-		public void Disable() {
-			_removeListeners ();
-		}
+        public void Enable()
+        {
+            _addListeners();
+        }
 
-		private void Awake() {
-			_addListeners ();
-		}
+        public void Disable()
+        {
+            _removeListeners();
+        }
 
-		private void OnDestroy() {
-			_removeListeners ();
-		}
+        private void Awake()
+        {
+            _addListeners();
+        }
 
-		private void _addListeners() {
-			if (!_isListenersAdded) {
-				_isListenersAdded = true;
-				EventCenter ec = EventCenter.Instance;
-				if (ec != null) {
-					ec.OnStringEvent += OnStringEvent;
-				}
-			}
-		}
+        private void OnDestroy()
+        {
+            _removeListeners();
+        }
 
-		private void _removeListeners() {
-			if (_isListenersAdded) {
-				_isListenersAdded = false;
-				EventCenter ec = EventCenter.Instance;
-				if (ec != null) {
-					ec.OnStringEvent -= OnStringEvent;
-				}
-			}
-		}
+        private void _addListeners()
+        {
+            if (!_isListenersAdded)
+            {
+                _isListenersAdded = true;
+                EventCenter ec = EventCenter.Instance;
+                if (ec != null)
+                {
+                    ec.OnStringEvent += OnStringEvent;
+                }
+            }
+        }
 
-		private void Log(string message) {
-			if(isLogOn) {
-				Debug.Log(message);
-			}
-		}
-	}
+        private void _removeListeners()
+        {
+            if (_isListenersAdded)
+            {
+                _isListenersAdded = false;
+                EventCenter ec = EventCenter.Instance;
+                if (ec != null)
+                {
+                    ec.OnStringEvent -= OnStringEvent;
+                }
+            }
+        }
+
+        private void Log(string message)
+        {
+            if (isLogOn)
+            {
+                Debug.Log(message);
+            }
+        }
+    }
 }
