32cfd7e5c077c42c19d5706a289f601d55172a00
Paul Hoover <polyphonic13@gmail.com>
2022-03-09 15:40:23 -0500

    fixes bedroom n bed canopy fabric material; CameraZoon disables cursor when zoomed

--- Assets/scripts/cameras/CameraZoom.cs
+++ Assets/scripts/cameras/CameraZoom.cs
@@ -1,36 +1,45 @@
 using UnityEngine;
 using Polyworks;
 
-public class CameraZoom : MonoBehaviour {
+public class CameraZoom : MonoBehaviour
+{
 
-	public float _zoom = 10;
+    public float Zoom = 10;
+    public GameObject Cursor;
 
-	private Camera _camera;
-	private float _normal;
-	private bool _isZoomed;
+    private Camera target;
+    private float normal;
+    private bool isZoomed = false;
 
-	public void Execute() {
-		_zoomCamera ();
-	}
+    public void Execute()
+    {
+        zoomCamera();
+    }
 
-	public void OnMainCameraEnabled() {
-		_camera = Camera.main;
-		_normal = _camera.fieldOfView;
-	}
+    public void OnMainCameraEnabled()
+    {
+        target = Camera.main;
+        normal = target.fieldOfView;
+    }
 
-	private void Awake() {
-		EventCenter.Instance.OnMainCameraEnabled += OnMainCameraEnabled;
-	}
-		
-	private void _zoomCamera() {
-		_camera.fieldOfView =(_isZoomed) ? _normal : _zoom;
-		_isZoomed = !_isZoomed;
-	}
+    private void Awake()
+    {
+        EventCenter.Instance.OnMainCameraEnabled += OnMainCameraEnabled;
+    }
 
-	private void OnDestroy() {
-		EventCenter ec = EventCenter.Instance;
-		if (ec != null) {
-			ec.OnMainCameraEnabled -= OnMainCameraEnabled;
-		}
-	}
+    private void zoomCamera()
+    {
+        target.fieldOfView = (isZoomed) ? normal : Zoom;
+        isZoomed = !isZoomed;
+        Cursor.SetActive(!isZoomed);
+    }
+
+    private void OnDestroy()
+    {
+        EventCenter ec = EventCenter.Instance;
+        if (ec != null)
+        {
+            ec.OnMainCameraEnabled -= OnMainCameraEnabled;
+        }
+    }
 }
